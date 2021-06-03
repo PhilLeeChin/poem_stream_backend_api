180a695e5e67d692ea7eb4164337f400e2902aa6
PhilLeeChin <phil.leechin@outlook.com>
2021-06-02 22:44:43 -0400

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,13 @@ sad = Genre.create(name: "Sad")
 happy = Genre.create(name: "Happy")
 dispair = Genre.create(name: "Dispair")
 
-poem_objs = [
-    {title: 'Emotionless', body: 'A void between two worlds that darkness sit, no pain, no happiness, no saddness.', genre_id: dispair.id},
-    {title: 'Giving up', body: 'A choice, a feeling, some say it is destiny, some say it is faith .... is it not the same thing.', genre_id: dispair.id},
-    {title: 'Admiration', body: 'As I visualize your design, your ambitious, and motivated, just a few quailties that makes you who you are.', genre_id: happy.id},
-    {title: 'Tree', body: 'Strong yet vulnerable, nurished by water and sunlight, but still needs love and care.', genre_id: sad.id}
-]
+poems = Poem.create(
+    [
+        {title: 'Emotionless', lines: 'A void between two worlds that darkness sit, no pain, no happiness, no saddness.', genre_id: dispair.id},
+        {title: 'Giving up', lines: 'A choice, a feeling, some say it is destiny, some say it is faith .... is it not the same thing.', genre_id: dispair.id},
+        {title: 'Admiration', lines: 'As I visualize your design, your ambitious, and motivated, just a few quailties that makes you who you are.', genre_id: happy.id},
+        {title: 'Tree', lines: 'Strong yet vulnerable, nurished by water and sunlight, but still needs love and care.', genre_id: sad.id}
+    ]
+)
 
-poem_objs.each {|hash| Poem.create(hash)}
+# poem_objs.each {|hash| Poem.create(hash)}

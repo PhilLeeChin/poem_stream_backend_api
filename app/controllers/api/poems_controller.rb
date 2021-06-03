5cc623d63837097f9e550a328ab34fd8b3e73c4c
PhilLeeChin <phil.leechin@outlook.com>
2021-06-03 00:48:35 -0400

    add serializer code to Poem controller

--- app/controllers/api/poems_controller.rb
+++ app/controllers/api/poems_controller.rb
@@ -3,11 +3,18 @@ class Api::PoemsController < ApplicationController
     def index
         poems = Poem.all
 
-        render json: poems
+        # render json: poems
+        render json: PoemSerializer.new(poems)
     end
 
     def create
         po_obj = Poem.new(poem_params)
+        if Poem.save
+            render json: po_obj, status: :accepted
+        else
+            render json: {errors: po_obj.errors.full_messages}, status:
+            :unprocessible_entity
+        end
     end
 
     # def show
@@ -20,6 +27,6 @@ class Api::PoemsController < ApplicationController
     # end
 
     def poem_params
-        params.require(:poem).permit{:title, :lines, :genre_id}
+        params.require(:poem).permit(:title, :lines, :genre_id)
     end
 end

21529bebde426b6928dc9f1452503d2d4c0f40bb
PhilLeeChin <phil.leechin@outlook.com>
2021-06-06 00:06:52 -0400

    add show method

--- app/controllers/api/genres_controller.rb
+++ app/controllers/api/genres_controller.rb
@@ -4,4 +4,9 @@ class GenresController < ApplicationController
     #render json: genres
     render json: GenreSerializer.new(genre)
   end
+
+  # def show
+  #   genre = Genre.find(params[:id])
+  #   render json: genre
+  # end
 end

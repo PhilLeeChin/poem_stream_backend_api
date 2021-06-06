9174b794b1e122cf920635b2cc8ec241c11131c4
PhilLeeChin <phil.leechin@outlook.com>
2021-06-06 00:07:12 -0400

    add destroy method

--- app/controllers/api/poems_controller.rb
+++ app/controllers/api/poems_controller.rb
@@ -6,9 +6,14 @@ class Api::PoemsController < ApplicationController
         render json: PoemSerializer.new(poems)
     end
 
+    def show
+        poem = Poem.find(params[:id])
+        render json: PoemSerializer.new(poem)
+    end
+
     def create
         po_obj = Poem.new(poem_params)
-        if Poem.save
+        if po_obj.save
             render json: po_obj, status: :accepted
         else
             render json: {errors: po_obj.errors.full_messages}, status:
@@ -16,9 +21,12 @@ class Api::PoemsController < ApplicationController
         end
     end
 
-    # def show
-    #     render json: @poem
-    # end
+   def destroy
+        poem = Poem.find(params[:id])
+        if poem.destroy
+            render json: {id: poem.id}
+        end
+   end
 
     def poem_params
         params.require(:poem).permit(:title, :lines, :genre_id)

9cbab1dff3008df68c8a3dcbfdf2ad0b46771bf1
PhilLeeChin <phil.leechin@outlook.com>
2021-06-06 00:09:02 -0400

    update routes for poem and genre

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'genres/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
-    resources :poems, only: [:index, :create]
-    resources :genres, only: [:index]
+    resources :poems, only: [:index, :create, :destroy, :show]
+    resources :genres, only: [:index, :show]
   end
 end
